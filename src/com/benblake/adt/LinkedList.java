e5235a65d9bd72c5e34b6f1ca69720080e7f460c
Ben Blake <benblake019@gmail.com>
2018-03-25 21:29:10 -0400

    Completion of basic LinkedList: O(1) add, O(N) get, O(N) insert.

--- src/com/benblake/adt/LinkedList.java
+++ src/com/benblake/adt/LinkedList.java
@@ -1,10 +1,54 @@
 package com.benblake.adt;
 
-public class LinkedList {
+public class LinkedList<T> {
 
+    private int mLength;
+    private Node<T> mRootNode;
+    private Node<T> mEndNode;
 
+    public T get(int index) {
+        if (index >= mLength) {
+            return null;
+        }
+
+        Node<T> temp = mRootNode;
+        for (int i = 0; i < index; i++) {
+            temp = temp.getNextNode();
+        }
+        return temp.getData();
+    }
+
+    public void add(T data) {
+        if (mLength == 0) {
+            mRootNode = new Node<>(data);
+            mEndNode = mRootNode;
+        } else {
+            mEndNode.setNextNode(new Node<>(data));
+            mEndNode = mEndNode.getNextNode();
+        }
+        mLength++;
+    }
+
+    public void insert(int index, T data) {
+        if (index == 0) {
+            mRootNode = new Node<>(data, mRootNode);
+            mLength++;
+        } else if (index <= mLength) {
+            Node<T> temp = mRootNode;
+            for (int i = 0; i < (index - 1); i++) {
+                temp = temp.getNextNode();
+            }
+            temp.setNextNode(new Node<>(data, temp.getNextNode()));
+            mLength++;
+        }
+    }
+
+    public int getLength() {
+        return mLength;
+    }
 
     public static class Node<T> {
+
         private T mData;
         private Node<T> mNextNode;
 
@@ -23,6 +67,10 @@ public class LinkedList {
 
         public Node<T> getNextNode() {
             return mNextNode;
+        }
+
+        public void setNextNode(Node<T> nextNode) {
+            mNextNode = nextNode;
         }
 
         @Override
--- test/com/benblake/adt/LinkedListTest.java
+++ test/com/benblake/adt/LinkedListTest.java
@@ -6,6 +6,7 @@ import static com.benblake.adt.LinkedList.*;
 import static org.junit.Assert.*;
 
 public class LinkedListTest {
+
     @Test
     public void nodeConstructorTakesGeneralObjectDataAndDefaultsNextNodeToNull() {
         Object data = new Object();
@@ -20,6 +21,17 @@ public class LinkedListTest {
         Object data = new Object();
         Node<Object> nextNode = new Node<>(data);
         Node<Object> node = new Node<>(data, nextNode);
+
+        assertEquals(nextNode, node.getNextNode());
+    }
+
+    @Test
+    public void settingNextNodeSetsCorrectly() {
+        Object data1 = new Object();
+        Object data2 = new Object();
+        Node<Object> node = new Node<>(data1);
+        Node<Object> nextNode = new Node<>(data2);
+        node.setNextNode(nextNode);
 
         assertEquals(nextNode, node.getNextNode());
     }
@@ -51,5 +63,46 @@ public class LinkedListTest {
         Node<Integer> integerNode3 = new Node<>(integerData2, integerNode1);
 
         assertEquals(integerNode2, integerNode3);
+    }
+
+    @Test
+    public void emptyLinkedListReturnsNullForFirstElement() {
+        LinkedList<Object> linkedList = new LinkedList<>();
+
+        assertNull(linkedList.get(0));
+    }
+
+    @Test
+    public void threeEntryLinkedListCreatedFromAddingReturnsCorrectNodeAndLengthData() {
+        Integer data1 = new Integer(1);
+        Integer data2 = new Integer(2);
+        Integer data3 = new Integer(3);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.add(data1);
+        linkedList.add(data2);
+        linkedList.add(data3);
+
+        assertEquals(data1, linkedList.get(0));
+        assertEquals(data2, linkedList.get(1));
+        assertEquals(data3, linkedList.get(2));
+        assertEquals(null, linkedList.get(3));
+        assertEquals(3, linkedList.getLength());
+    }
+
+    @Test
+    public void threeEntryLinkedListCreatedFromInsertingReturnsCorrectNodeAndLengthData() {
+        Integer data1 = new Integer(1);
+        Integer data2 = new Integer(2);
+        Integer data3 = new Integer(3);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        linkedList.insert(0, data1);
+        linkedList.insert(0, data2);
+        linkedList.insert(1, data3);
+
+        assertEquals(data2, linkedList.get(0));
+        assertEquals(data3, linkedList.get(1));
+        assertEquals(data1, linkedList.get(2));
+        assertEquals(null, linkedList.get(3));
+        assertEquals(3, linkedList.getLength());
     }
 }
