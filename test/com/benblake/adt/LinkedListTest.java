60c1277e0876c4a9d3572e53a44549bb82b55b40
Ben Blake <benblake019@gmail.com>
2018-03-25 20:11:15 -0400

    Completed Node inner class for LinkedList.

---
+++ test/com/benblake/adt/LinkedListTest.java
@@ -0,0 +1,55 @@
+package com.benblake.adt;
+
+import org.junit.Test;
+
+import static com.benblake.adt.LinkedList.*;
+import static org.junit.Assert.*;
+
+public class LinkedListTest {
+    @Test
+    public void nodeConstructorTakesGeneralObjectDataAndDefaultsNextNodeToNull() {
+        Object data = new Object();
+        Node<Object> node = new Node<>(data);
+
+        assertEquals(data, node.getData());
+        assertEquals(null, node.getNextNode());
+    }
+
+    @Test
+    public void alternateNodeConstructorTakesPointerToNextNode() {
+        Object data = new Object();
+        Node<Object> nextNode = new Node<>(data);
+        Node<Object> node = new Node<>(data, nextNode);
+
+        assertEquals(nextNode, node.getNextNode());
+    }
+
+    @Test
+    public void nodeCannotEqualObjectOfDifferentType() {
+        Object data = new Object();
+        Node<Object> node = new Node<>(data);
+
+        assertNotEquals(node, data);
+    }
+
+    @Test
+    public void nodeCannotEqualObjectOfDifferentGenericType() {
+        Integer integerData = new Integer(6);
+        Float floatData = new Float(6);
+        Node<Integer> integerNode = new Node<>(integerData);
+        Node<Float> floatNode = new Node<>(floatData);
+
+        assertNotEquals(integerNode, floatNode);
+    }
+
+    @Test
+    public void nodeEqualsNodeWithSameDataAndNextNode() {
+        Integer integerData1 = new Integer(6);
+        Integer integerData2 = new Integer(6);
+        Node<Integer> integerNode1 = new Node<>(integerData1);
+        Node<Integer> integerNode2 = new Node<>(integerData1, integerNode1);
+        Node<Integer> integerNode3 = new Node<>(integerData2, integerNode1);
+
+        assertEquals(integerNode2, integerNode3);
+    }
+}
